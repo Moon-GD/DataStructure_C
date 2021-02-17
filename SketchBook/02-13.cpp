02fde74bd1f7e93eb95845b92b7b01d7e6182d76
PC-GD\문경덕 <king4mun@gmail.com>
2021-02-17 11:44:12 +0900

    5th

--- SketchBook/02-13.cpp
+++ SketchBook/02-13.cpp
@@ -5,15 +5,58 @@ typedef struct {
 	int d;
 }Date;
 
+int month[2][13] = { {0,31,28,31,30,31,30,31,31,30,31,30,31},
+	{0,31,29,31,30,31,30,31,31,30,31,30,31} };
 
+int isleap(int y) {
+	return y % 4 == 0 && y % 100 != 0 || y % 400 == 0;
+}
+
+Date Dateof(int y, int m, int d) {
+	Date dateof;
+	dateof.d = d;
+	dateof.m = m;
+	dateof.y = y;
+	return dateof;
+}
+
+Date After(Date x, int n) {
+	x.d += n;
+	while (x.d >= month[isleap(x.y)][x.m]) {
+		x.d -= month[isleap(x.y)][x.m];
+		if (++x.m > 12) {
+			x.m = 1;
+			x.y++;
+		}
+	}
+	return x;
+}
+
+Date Before(Date x, int n) {
+	x.d -= n;
+	while (x.d < 1) {
+		if (--x.m < 1) {
+			x.m = 12;
+			x.y--;
+		}
+		x.d += month[isleap(x.y)][x.m];
+	}
+	return x;
+}
 
 int main() {
 	Date date;
-
+	int n = 0;
 	printf("몇 년입니까? "); scanf_s("%d", &date.y);
 	printf("몇 월입니까? "); scanf_s("%d", &date.m);
 	printf("몇 일입니까? "); scanf_s("%d", &date.d);
-	printf("입력하신 시각은 %d년 %d2월 %2d일 입니다.", date.y, date.m, date.d);
+	/*
+	printf("몇 일 후입니까? "); scanf_s("%d", &n);
+	date = After(date, n);
+	*/
+	printf("몇 일 전입니까? "); scanf_s("%d", &n);
+	date = Before(date, n);
+	printf("%d년 %d월 %d일 입니다.", date.y, date.m, date.d);
 
 	return 0;
 }
